2cad76a191fcd736cfc7a3003b53ca1eb71b5928
Richard Patel <me@terorie.dev>
2019-04-26 14:24:24 +0200

    Remove test vectors

--- tests/ed25519.rs
+++ tests/ed25519.rs
@@ -10,68 +10,12 @@
 //! Integration tests for ed25519-dalek.
 
 extern crate ed25519_signal;
-extern crate hex;
 extern crate rand;
 
 use ed25519_signal::*;
 
-use hex::FromHex;
-
 use rand::thread_rng;
 use rand::rngs::ThreadRng;
-
-#[cfg(test)]
-mod vectors {
-    use std::io::BufReader;
-    use std::io::BufRead;
-    use std::fs::File;
-
-    use super::*;
-
-    // TESTVECTORS is taken from sign.input.gz in agl's ed25519 Golang
-    // package. It is a selection of test cases from
-    // http://ed25519.cr.yp.to/python/sign.input
-    #[test]
-    fn against_reference_implementation() { // TestGolden
-        let mut line: String;
-        let mut lineno: usize = 0;
-
-        let f = File::open("TESTVECTORS");
-        if f.is_err() {
-            println!("This test is only available when the code has been cloned \
-                      from the git repository, since the TESTVECTORS file is large \
-                      and is therefore not included within the distributed crate.");
-            panic!();
-        }
-        let file = BufReader::new(f.unwrap());
-
-        for l in file.lines() {
-            lineno += 1;
-            line = l.unwrap();
-
-            let parts: Vec<&str> = line.split(':').collect();
-            assert_eq!(parts.len(), 5, "wrong number of fields in line {}", lineno);
-
-            let sec_bytes: Vec<u8> = FromHex::from_hex(&parts[0]).unwrap();
-            let pub_bytes: Vec<u8> = FromHex::from_hex(&parts[1]).unwrap();
-            let msg_bytes: Vec<u8> = FromHex::from_hex(&parts[2]).unwrap();
-            let sig_bytes: Vec<u8> = FromHex::from_hex(&parts[3]).unwrap();
-
-            let secret: SecretKey = SecretKey::from_bytes(&sec_bytes[..SECRET_KEY_LENGTH]).unwrap();
-            let public: PublicKey = PublicKey::from_bytes(&pub_bytes[..PUBLIC_KEY_LENGTH]).unwrap();
-            let keypair: Keypair  = Keypair{ secret: secret, public: public };
-
-		    // The signatures in the test vectors also include the message
-		    // at the end, but we just want R and S.
-            let sig1: Signature = Signature::from_bytes(&sig_bytes[..64]).unwrap();
-            let sig2: Signature = keypair.sign(&msg_bytes);
-
-            assert!(sig1 == sig2, "Signature bytes not equal on line {}", lineno);
-            assert!(keypair.verify(&msg_bytes, &sig2).is_ok(),
-                    "Signature verification failed on line {}", lineno);
-        }
-    }
-}
 
 #[cfg(test)]
 mod integrations {
