60918152216cf23266775c4eea0536cccca22076
Richard Patel <me@terorie.dev>
2019-04-26 02:06:05 +0200

    Add C bindings

---
+++ src/ffi.rs
@@ -0,0 +1,47 @@
+extern crate libc;
+
+use std::os::raw::*;
+
+extern "C" {
+    pub fn curve25519_donna(
+        public: *mut c_uchar,
+        secret: *const c_uchar,
+        base_point: *const c_uchar,
+    ) -> c_int;
+
+    pub fn xed25519_sign(
+        signature_out: *mut c_uchar,
+        curve25519_privkey: *const c_uchar,
+        msg: *const c_uchar,
+        msg_len: c_ulong,
+        random: *const c_uchar,
+    ) -> c_int;
+
+    pub fn xed25519_verify(
+        signature: *const c_uchar,
+        curve25519_pubkey: *const c_uchar,
+        msg: *const c_uchar,
+        msg_len: c_ulong,
+    ) -> c_int;
+
+    pub fn generalized_veddsa_25519_sign(
+        signature_out: *mut c_uchar,
+        eddsa_25519_pubkey_bytes: *const c_uchar,
+        eddsa_25519_privkey_scalar: *const c_uchar,
+        msg: *const c_uchar,
+        msg_len: c_ulong,
+        random: *const c_uchar,
+        customization_label: *const c_uchar,
+        customization_label_len: c_ulong,
+    ) -> c_int;
+
+    pub fn generalized_veddsa_25519_verify(
+        vrf_out: *mut c_uchar,
+        signature: *const c_uchar,
+        eddsa_25519_pubkey_bytes: *const c_uchar,
+        msg: *const c_uchar,
+        msg_len: c_ulong,
+        customization_label: *const c_uchar,
+        customization_label_len: c_ulong,
+    ) -> c_int;
+}
