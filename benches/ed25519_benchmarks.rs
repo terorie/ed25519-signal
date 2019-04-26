af0575a5e15e7feaf90599fd32d819a17c96bcaf
Richard Patel <me@terorie.dev>
2019-04-27 00:04:30 +0200

    Add VXEdDSA benchmarks

--- benches/ed25519_benchmarks.rs
+++ benches/ed25519_benchmarks.rs
@@ -18,6 +18,7 @@ mod ed25519_benches {
     use super::*;
     use ed25519_signal::Keypair;
     use ed25519_signal::Signature;
+    use ed25519_signal::SignatureVRF;
     use rand::thread_rng;
     use rand::rngs::ThreadRng;
 
@@ -26,8 +27,8 @@ mod ed25519_benches {
         let keypair: Keypair = Keypair::generate(&mut csprng);
         let msg: &[u8] = b"";
 
-        c.bench_function("Ed25519 signing", move |b| {
-                         b.iter(| | keypair.sign(msg))
+        c.bench_function("XEd25519 signing", move |b| {
+            b.iter(| | keypair.sign(msg))
         });
     }
 
@@ -37,10 +38,33 @@ mod ed25519_benches {
         let msg: &[u8] = b"";
         let sig: Signature = keypair.sign(msg);
         
-        c.bench_function("Ed25519 signature verification", move |b| {
-                         b.iter(| | keypair.verify(msg, &sig))
+        c.bench_function("XEd25519 signature verification", move |b| {
+            b.iter(| | keypair.verify(msg, &sig))
         });
     }
+
+    fn sign_vrf(c: &mut Criterion) {
+        let mut csprng: ThreadRng = thread_rng();
+        let keypair: Keypair = Keypair::generate(&mut csprng);
+        let msg: &[u8] = b"";
+        let label: &[u8] = b"";
+
+        c.bench_function("VXEd25519 signing", move |b| {
+            b.iter(| | keypair.sign_vrf(msg, label))
+        });
+    }
+
+    fn verify_vrf(c: &mut Criterion) {
+        let mut csprng: ThreadRng = thread_rng();
+        let keypair: Keypair = Keypair::generate(&mut csprng);
+        let msg: &[u8] = b"";
+        let label: &[u8] = b"";
+        let sig: SignatureVRF = keypair.sign_vrf(msg, label);
+
+        c.bench_function("VXEd25519 signature verification", move |b| {
+            b.iter(| | keypair.verify_vrf(msg, label, &sig))
+        });
+}
 
     fn key_generation(c: &mut Criterion) {
         let mut csprng: ThreadRng = thread_rng();
@@ -56,6 +80,8 @@ mod ed25519_benches {
         targets =
             sign,
             verify,
+            sign_vrf,
+            verify_vrf,
             key_generation,
     }
 }
